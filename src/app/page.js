9d80dd150ad768cf8303b9a5cfa5f25f450aa07e
melchanatan <chanatan@outlook.com>
2024-02-26 23:08:11 +0700

    feat: added hero section image

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      hello world
+     
+      <Image src="/newsiya-logo.svg" alt="Vercel Logo" width={72} height={16} />
+      <div className="bg-[url('/hero-img.png')] w-full h-[80vh] bg-no-repeat bg-center aspect-auto">
+
+      </div>
     </main>
   );
 }
