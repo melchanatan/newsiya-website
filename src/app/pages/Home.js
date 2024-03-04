7817fe98aa5d533cbc9df7e7c79527e1cb772886
mels <chanatan.melmel@gmail.com>
2024-03-04 19:00:21 +0700

    fix: map section responsiveness

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -91,9 +91,9 @@ return (
     <ReviewsSlider />
 
     </section>
-<section id="maps" className="flex gap-[5vw] bg-primary !py-0">
-  <MyMap className="w-[60vw] h-[40vh] lg:w-[50vw]" />
-  <div className="mt-[4vh] ">
+<section id="maps" className="flex flex-col sm:flex-row gap-[5vw] bg-primary !py-0">
+  <MyMap className="w-full sm:w-[60vw] h-[40vh] lg:w-[50vw]" />
+  <div className="mt-[4vh] sm:w-[30vw] order-[-1] sm:order-[2] padding_page sm:px-0">
     <h1 className="heading mb-5">Contacts</h1>
     <p className="w-[95%] md:w-[36ch]">
       ไอซียู อ่อนด้อยเอเซีย นิวหมั่นโถว แอปเปิ้ล เคลื่อนย้าย ตุ๊กตุ๊กศากยบุตรหน่อมแน้ม เฮียไวอากร้าพาร์ทเนอร์ ฮวงจุ้ยบาร์บี้อุรังคธาตุ มิวสิคกุมภาพันธ์ไฮไลต์ สตริงสามแยกแครอทกระดี๊กระด๊ากระดี๊กระด๊า
