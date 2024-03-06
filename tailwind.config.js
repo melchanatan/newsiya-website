b864b6f1cbe1e134cca242a4ab7592842f50f236
melchanatan <chanatan@outlook.com>
2024-03-06 23:17:51 +0700

    feat: added loading animation on WithSplashscreen.jsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,23 +8,31 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        outfit: ["Outfit", 'sans-serif'],
-        maehongson: ["var(--font-maehongson)", 'sans-serif']
+        outfit: ["Outfit", "sans-serif"],
+        maehongson: ["var(--font-maehongson)", "sans-serif"],
       },
       colors: {
         "accent-darken": "#333B44",
-        "primary": "#FFFAF3",
-        "primary-darken": "#C3AC93"
+        primary: "#FFFAF3",
+        "primary-darken": "#C3AC93",
       },
       fontSize: {
         "hero-clamp": "clamp(2rem, 4vw, 4rem)",
         "title-clamp": "clamp(2rem, 4vw, 3rem)",
-
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        pulsate: {
+          "0%, 100%": { transform: "scale(95%)" },
+          "50%": { transform: "scale(105%)" },
+        },
+      },
+      animation: {
+        pulsate: "pulsate 3s ease-in-out infinite",
       },
     },
   },
