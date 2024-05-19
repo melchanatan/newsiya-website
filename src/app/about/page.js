6f035f16e862d8baa99ad664fd03d10ebd5b8246
mels <chanatan.melmel@gmail.com>
2024-05-19 23:16:58 +0700

    feat: added contact and pricing route

---
+++ src/app/about/page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+const page = () => {
+  return (
+    <section id="travel" className="bg-primary mt-20 align-center justify-center flex flex-row gap-[3vw] items-center">
+      <Image
+        className="rounded-[50px] border-primary border-[3px] h-[50vh]" src='/hero-img.png'
+        width={500}
+        height={400}
+        style={{ objectFit: "cover" }}
+        alt='near by travel location'
+      ></Image>
+      <div>
+        <h1 className="heading mb-5">About Us</h1>
+        <p className='w-[36ch]'>
+          ไอซียู อ่อนด้อยเอเซีย นิวหมั่นโถว แอปเปิ้ล เคลื่อนย้าย ตุ๊กตุ๊กศากยบุตรหน่อมแน้ม เฮียไวอากร้าพาร์ทเนอร์ ฮวงจุ้ยบาร์บี้อุรังคธาตุ มิวสิคกุมภาพันธ์ไฮไลต์ สตริงสามแยกแครอทกระดี๊กระด๊ากระดี๊กระด๊าไอซียู อ่อนด้อยเอเซีย นิวหมั่นโถว แอปเปิ้ล เคลื่อนย้าย ตุ๊กตุ๊กศากยบุตรหน่อมแน้ม เฮียไวอากร้าพาร์ทเนอร์ ฮวงจุ้ยบาร์บี้อุรังคธาตุ มิวสิคกุมภาพันธ์ไฮไลต์ สตริงสามแยกแครอทกระดี๊กระด๊ากระดี๊กระด๊า
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default page
---
+++ src/app/pricing/page.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+const page = () => {
+    return (
+        <section id="pricing" className="bg-primary mt-20 align-center justify-center flex flex-col items-center">
+            <Image
+                className="rounded-[50px] border-primary border-[3px] h-[30vh]" src='/hero-img.png'
+                width={400}
+                height={400}
+                style={{objectFit:"cover"}}
+                alt='near by travel location'
+            ></Image>
+            <table>
+                <tr>
+                    <th>ประเภท</th>
+                    <th>ประเภท</th>
+                    <th>ประเภท</th>
+                </tr>
+                <tr>
+                    <td>100px</td>
+                </tr>
+            </table>
+        </section>
+    )
+}
+
+export default page
