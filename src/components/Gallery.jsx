f4b6c8164b21d1b4a8958339bfb9b0bf6ba44db5
melchanatan <chanatan@outlook.com>
2024-06-12 20:21:10 +0700

    feat: implement contentful with /travel route

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,6 +4,7 @@ import LightGallery from 'lightgallery/react';
 import { FaExpandAlt } from "react-icons/fa";
 
 // import styles
+import './Gallery.css'
 import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
 import 'lightgallery/css/lg-thumbnail.css';
@@ -12,7 +13,7 @@ import lgThumbnail from 'lightgallery/plugins/thumbnail';
 import lgZoom from 'lightgallery/plugins/zoom';
 
 
-const Gallery = ({images}) => {
+const Gallery = ({ images }) => {
     return (
         <LightGallery
             elementClassNames='flex gap-3 flex-wrap padding_page'
@@ -21,13 +22,39 @@ const Gallery = ({images}) => {
         >
             {
                 images.map((image, index) => (
-                    <a key={index} href={image.src} className='relative'>
+                    <a
+                        key={index}
+                        href={image.src}
+                        className='relative'
+                        data-sub-html={`
+                        <h4 
+                            style="
+                                font-size: 2rem;
+                                font-family: 'maehongson';
+                                transform: translate(0, -7rem);
+                            "
+                        >
+                            ${image.alt}
+                        </h4>
+                        <p
+                            style="
+                                font-size: 1.4rem;
+                                font-family: 'maehongson';
+                                transform: translate(0, -7rem);
+                            "
+                        >
+                            ${image.description}
+                        </p>
+                        `}
+                    >
                         <div className='group [&>*]:transition-all [&>*]:duration-300'>
                             <span className='w-full h-full top-0 left-0 absolute bg-black opacity-0 group-hover:opacity-30 '></span>
                             <span className='p-6 bg-white rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100'><FaExpandAlt className='w-6 h-6' /></span>
                         </div>
-                        <img alt="img1" src={image.src} />
+                        
+                        <img src={image.src} />
                     </a>
+
                 ))
             }
         </LightGallery>
