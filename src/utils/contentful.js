591f215ef9566e889a791bb49fdeea243e55fabb
mels <chanatan.melmel@gmail.com>
2024-07-04 20:11:10 +0700

    test: change revalidation time to 10 second on contentful.js

--- src/utils/contentful.js
+++ src/utils/contentful.js
@@ -1,24 +1,22 @@
 import * as contentful from "contentful";
-import { cache } from 'react'
+import { cache } from "react";
 
-const entryIdHero = "6nVvZP2GObxuQXLEf6xBgm"
-const entryIdAbout = "1fswxyxIb0PhjQXJfebeT2"
-const entryIdServices = "3hlrDsCxw3NsQkskwEH48e"
-const entryIdTravel = "7cCCWmyi4INFpRvkAPB11e"
-const entryIdPricing = "4iFiYUzanbJFuG9a7LdQ2W"
-const entryIdContact = "3MZdFoHejJSUHtRNTsKbx0"
-
-
+const entryIdHero = "6nVvZP2GObxuQXLEf6xBgm";
+const entryIdAbout = "1fswxyxIb0PhjQXJfebeT2";
+const entryIdServices = "3hlrDsCxw3NsQkskwEH48e";
+const entryIdTravel = "7cCCWmyi4INFpRvkAPB11e";
+const entryIdPricing = "4iFiYUzanbJFuG9a7LdQ2W";
+const entryIdContact = "3MZdFoHejJSUHtRNTsKbx0";
 
 const client = contentful.createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });
 
-export const revalidate = 3600; 
+export const revalidate = 10;
 
 export const getContentHero = cache(async () => {
-  const result = await client.getEntry(entryIdHero)
+  const result = await client.getEntry(entryIdHero);
   return {
     images: result.fields.images,
     title: result.fields.title,
@@ -27,41 +25,41 @@ export const getContentHero = cache(async () => {
     agodaRating: result.fields.agodaRating,
     reviews: result.fields.reviews,
     promotionImages: result.fields.promotionImages,
-  }
-})
+  };
+});
 
 export const getContentAbout = cache(async () => {
-  const result = await client.getEntry(entryIdAbout)
+  const result = await client.getEntry(entryIdAbout);
   return {
     image: result.fields.image,
     text: result.fields.text,
-  }
-})
+  };
+});
 
 export const getContentServices = cache(async () => {
-  const result = await client.getEntry(entryIdServices)
+  const result = await client.getEntry(entryIdServices);
   return {
-    serviceImages: result.fields.serviceImages
-  }
-})
+    serviceImages: result.fields.serviceImages,
+  };
+});
 
 export const getContentTravel = cache(async () => {
-  const result = await client.getEntry(entryIdTravel)
+  const result = await client.getEntry(entryIdTravel);
   return {
-    images: result.fields.images
-  }
-})
+    images: result.fields.images,
+  };
+});
 
 export const getContentPricing = cache(async () => {
-  const result = await client.getEntry(entryIdPricing)
+  const result = await client.getEntry(entryIdPricing);
   return {
-    images: result.fields.images
-  }
-})
+    images: result.fields.images,
+  };
+});
 
 export const getContentContact = cache(async () => {
-  const result = await client.getEntry(entryIdContact)
+  const result = await client.getEntry(entryIdContact);
   return {
-    description: result.fields.description
-  }
-})
+    description: result.fields.description,
+  };
+});
